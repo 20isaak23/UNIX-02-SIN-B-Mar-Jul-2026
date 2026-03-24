0a22ae2d8b03dcbe60d63d14de3eae063a09c550
20isaak23 <sakabenalcazar@gmail.com>
2026-03-24 23:08:01 +0000

    Block C: Create file and encrypt it for partner

--- script_gnpg.sh
+++ script_gnpg.sh
@@ -6,3 +6,5 @@ gpg --list-secret-keys --keyid-format=long
 gpg --armor --export 7F946AAB2BE9B927 > mi_llave_publica.asc
 gpg --import llave_publica_henry.asc 
 gpg --list-keys 
+echo (" Mensaje secreto") > doc_no_cifrado.txt 
+gpg --encrypt --recipient chineado12@gmail.com doc_no_cifrado.txt
