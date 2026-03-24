0472397f98a71fda3b95ae212cd9ae440a0faf26
20isaak23 <sakabenalcazar@gmail.com>
2026-03-24 21:40:17 +0000

    Block  A: Check GPG version

--- script_gnpg.sh
+++ script_gnpg.sh
@@ -1,18 +1,2 @@
-comados
-gpg --list-secret-keys
-Este comando es similar, como si abrieras un cajo de tu 
-escritorio par saber cuantas llaves tienes, dice el nombre del
-dueño el correo electronico y un codigo largo ID
-
-gpg --armor --export-secret-keys
-Nota: Si solo tiene un par de llaves este es suficiente 
---export-secret-keys: Se usa cuando quieres pasar tu llave 
-a otra computadora 
---armor: 
-
-gpg --list-keys
-lista todas las llaves publicas
-
-gpg --import llave_publica_Israel.asc 
-
-echo "este mensaje es secreto" > doc_no_cifrado.txt 
+# comados ejecutados 
+gpg --version 
