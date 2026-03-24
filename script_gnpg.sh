acf89651d050547b563f916827e908caf2045431
20isaak23 <sakabenalcazar@gmail.com>
2026-03-24 22:57:20 +0000

    Block B: Import partner´s key and verify

--- script_gnpg.sh
+++ script_gnpg.sh
@@ -2,3 +2,7 @@
 gpg --version 
 gpg --full-generate-key
 gpg --list-secret-keys --keyid-format=long
+
+gpg --armor --export 7F946AAB2BE9B927 > mi_llave_publica.asc
+gpg --import llave_publica_henry.asc 
+gpg --list-keys 
