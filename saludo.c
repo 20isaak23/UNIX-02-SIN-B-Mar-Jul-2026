d52e8872afd8b02e042eca210db060c2e3bd05bc
20isaak23 <sakabenalcazar@gmail.com>
2026-03-25 13:04:56 +0000

    Practice Saludo C

---
+++ saludo.c
@@ -0,0 +1,10 @@
+/*Comandos de C*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hola UIDE, ya funciona!\n");
+    return 0;
+}
+/*Aqui hay varios comandos: Include: Carga las funciones basicas*/
+
