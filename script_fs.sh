0eb67ef2dd88f119f189b28a95145a7f2ca25376
20isaak23 <sakabenalcazar@gmail.com>
2026-03-30 13:06:13 +0000

    Part 2: Sudo parted

--- script_fs.sh
+++ script_fs.sh
@@ -2,3 +2,6 @@ sudo apt update #Update the packet list and version availables
 # in the respositores
 sudo apt upgrade #Install all the packets more recent
 sudo apt install parted #dowload and install parted
+sudo parted -l && echo -e "ln---\n" && lsblk -f && echo -e #
+#If the first part (mean since -l until &&) is true, follow
+# to next comand
