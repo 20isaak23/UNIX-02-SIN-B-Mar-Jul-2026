e46a744b6008d3446910f675b85c3d6fe7f3384f
20isaak23 <sakabenalcazar@gmail.com>
2026-03-30 12:57:32 +0000

    Part 1: Install parted

---
+++ script_fs.sh
@@ -0,0 +1,4 @@
+sudo apt update #Update the packet list and version availables 
+# in the respositores
+sudo apt upgrade #Install all the packets more recent
+sudo apt install parted #dowload and install parted
