e83c420e34988943566d4360c2032085ba7cf97c
20isaak23 <sakabenalcazar@gmail.com>
2026-04-06 12:34:19 +0000

    Change directory to home

---
+++ script_boot.sh
@@ -0,0 +1,6 @@
+boot
+cd: Change the directory root, create this symnol ~
+cd /cd /home/codespace: Acces to the folder of user specific 
+cd ~ : Return to folder of home 
+cd $HOME: change the driectory personal, we use the variable entorn
+  
