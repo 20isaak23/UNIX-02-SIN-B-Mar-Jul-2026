b08c6088ca52206358855c0ae65289d0789702ed
20isaak23 <sakabenalcazar@gmail.com>
2026-04-06 12:39:59 +0000

    List all the Inodos

--- script_boot.sh
+++ script_boot.sh
@@ -3,4 +3,6 @@ cd: Change the directory root, create this symnol ~
 cd /cd /home/codespace: Acces to the folder of user specific 
 cd ~ : Return to folder of home 
 cd $HOME: change the driectory personal, we use the variable entorn
-  
+Directory . -> Is hard link, represent the actual directory
+Directory .. -> Is hard link, represent the father directory 
+ls -lai -> List in format list, all the Inodos 
