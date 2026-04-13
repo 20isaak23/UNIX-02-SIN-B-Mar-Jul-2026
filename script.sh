40f4ef69a7a51d773a91c936f2f89e21301a9cec
20isaak23 <sakabenalcazar@gmail.com>
2026-04-13 12:43:29 +0000

    Part 3: Comand large and short

--- script.sh
+++ script.sh
@@ -1,3 +1,11 @@
 #Part 1 
 #Estructure = Command/ options/ arguments 
 ls -a and ls --all # List all the files and directories
+
+#Part2
+ls -l -a -h
+ls -l -ah
+ls -lah
+#The result is the same, the diference is: If you want save
+#script, write the command large if you want make to do fast, 
+#write the comand short. the result is the same in the three
