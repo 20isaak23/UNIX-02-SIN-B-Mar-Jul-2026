0b079467f4793a56371e0793d56b70c1f1f185c3
20isaak23 <sakabenalcazar@gmail.com>
2026-04-15 13:07:43 +0000

    ej = 2 Add the text priting

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -1,3 +1,4 @@
 echo '#!/bin/sh' > hola.sh #This command creates a file called hello.sh and inserts the "shebang" 
 #header which defines sh as the interpreter to run the script.
-echo 'echo "Hola desde mi primer script"' >> hola.sh #
+echo 'echo "Hola desde mi primer script"' >> hola.sh #Add the text printing instruction 
+#to the end of the hola.sh file without deleting the previous content
