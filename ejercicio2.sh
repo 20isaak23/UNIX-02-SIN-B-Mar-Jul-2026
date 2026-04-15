84e8130b91586a9ff81d2a63192f2a31c5742ca4
20isaak23 <sakabenalcazar@gmail.com>
2026-04-15 13:46:34 +0000

    Part 2:  prueba.txt

---
+++ ejercicio2.sh
@@ -0,0 +1,7 @@
+#Ejercicio 2
+touch prueba.txt #Create a empety file of prueba.txt
+chmod 600 prueba.txt #assigns permission 600 to the file, 
+#which means it is a private and exclusive access file.
+ls -l prueba.txt #This command list all the files of prueba.txt
+chmod 755 prueba.txt # Permit the acces to others 
+ls -l prueba.txt # Again list the try to prueba.txt
