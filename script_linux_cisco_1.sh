a78bd705a0f7097d6d040bf36f3a21a97f4d99d3
20isaak23 <sakabenalcazar@gmail.com>
2026-04-30 15:51:36 +0000

    2. ls

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,3 +1,10 @@
 #List of commands on linux
+#Part 1
 #This command lists the files and directories located in the Documents folder of the home directory. 
-ls ~/Documents 
+ls ~/Documents
+
+#Part 2
+#This command list all the files in this directory
+ls 
+
+
