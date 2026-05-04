8fcacafc067e9a047f9e1232d2145b8e853d9c27
20isaak23 <sakabenalcazar@gmail.com>
2026-05-04 12:59:50 +0000

    Unix_security: Teori

--- security.sh
+++ security.sh
@@ -3,3 +3,13 @@
 id
 #Read the system user file and display only the first 10 lines.
 cat /etc/passwd | head -10
+
+#Crackear: Hace una accion irreversible (lo hace a la fuera) y lo hace por medio de los hash 
+
+#¿ALmacena contraseñas en linux? -> No, almacena las hash de las contraseñas
+
+#¿Que es salt?
+#Es una cadena aleatoria que se genera cuando creas o cambias una contraseña 
+
+#Rainbow Tables
+#Tablas precomputadas de millones de pares (contraseña -> hash) sin salt
