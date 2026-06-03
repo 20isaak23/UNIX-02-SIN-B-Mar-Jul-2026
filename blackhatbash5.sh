d21fe62892fe018c34fa4c2b75f9e6173026cff4
20isaak23 <sakabenalcazar@gmail.com>
2026-06-03 13:33:09 +0000

    commands >,>>,&>

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -4,3 +4,10 @@ touch test && touch test123
 ls; ps; whoami
 lzl || echo "el comando lzl fallo"
 
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+
+ls -l / 1> stout.txt 2> stderr.txt
+lzl 2> error.txt
+cat error.txt
+
