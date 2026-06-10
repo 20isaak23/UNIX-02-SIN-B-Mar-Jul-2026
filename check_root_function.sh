ad19a35e886a8e50fcf24b3e75c29cb8b1f29868
20isaak23 <sakabenalcazar@gmail.com>
2026-06-10 13:23:18 +0000

    check_root_function. part 8

---
+++ check_root_function.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This function checks if the current user ID equals zero.
+check_if_root(){ #check_root_function.sh 
+if [[ "${EUID}" -eq "0" ]]; then #(EUID) Effective user ID this code use for know if is true or is false 
+ return 0
+ else
+ return 1
+ fi
+}
+if check_if_root; then
+ echo "User is root!"
+else
+ echo "User is not root!"
+fi
