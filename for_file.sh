f144660a120f8d0a746a9b30577ca7b44d8add1f
20isaak23 <sakabenalcazar@gmail.com>
2026-06-15 12:54:04 +0000

    for_ls

--- for_file.sh
+++ for_file.sh
@@ -1,8 +1,8 @@
 #! /bin/bash 
-for file in example_file*; do
+for file in example_file*; do # Here show a bucle
  if [[ "${file}" == "example_file1" ]]; then
- echo "Skipping the first file"
-continue
+ echo "Skipping the first file" #This condition is that example_file1 skip 
+continue #But the code follow with the command 
  fi
- echo "${RANDOM}" > "${file}"
+ echo "${RANDOM}" > "${file}" #Later print a random number 
 done
