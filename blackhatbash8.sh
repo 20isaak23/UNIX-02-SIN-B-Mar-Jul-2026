de743c099f6477236cabf214534f96afa6519a3f
20isaak23 <sakabenalcazar@gmail.com>
2026-06-17 12:54:05 +0000

    newlog

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -2,6 +2,12 @@
 awk '{print $1}' log.txt
 awk '{print $2}' log.txt
 awk '{print $3}' log.txt
+
+#All
 awk '{print $1,$2,$3}' log.txt
 
 awk 'NR < 10' log.txt
+
+grep "42.236.10.117" log.txt | awk '{print $7}'
+
+sed 's/Mozilla/Godzilla/g' log.txt
